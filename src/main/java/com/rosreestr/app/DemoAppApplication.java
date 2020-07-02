78dd533b012d51ea5f2d69d970dd3f457191ecda
scrag <scrag1987@gmail.com>
2020-07-02 18:49:26 +0600

    initial commit

---
+++ src/main/java/com/rosreestr/app/DemoAppApplication.java
@@ -0,0 +1,23 @@
+package com.rosreestr.app;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class DemoAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoAppApplication.class, args);
+  }
+
+  @Bean
+  public ApplicationRunner init() {
+    return args -> {
+     // for test purpose
+    };
+  }
+}
