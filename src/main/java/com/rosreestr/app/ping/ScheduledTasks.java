c12fd3733be85c562d0b63c1d5d0fb3418ad487b
scrag <scrag1987@gmail.com>
2020-07-06 13:21:25 +0600

    Fix delay check for apirosreestr.ru

--- src/main/java/com/rosreestr/app/ping/ScheduledTasks.java
+++ src/main/java/com/rosreestr/app/ping/ScheduledTasks.java
@@ -6,15 +6,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
 public class ScheduledTasks {
   private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
   private RestService restService;
-
+  volatile long delay;
   public ScheduledTasks(RestService restService) {
     this.restService = restService;
   }
@@ -37,9 +42,44 @@ public class ScheduledTasks {
   @Scheduled(cron = "${cron.expression}")
   public void scheduleTaskSecondaryServerAvailability() {
     log.info("Cron Dynamic Task: Current Time - {}", formatter.format(LocalDateTime.now()));
-    String ip = "apirosreestr.ru";
-    boolean reachable = RestService.isReachable(ip, 443, 2000);
-    log.info(ip + " isReacheble - " + reachable);
-    Status.updateStatusServer(70, reachable, false); // FIXME: 7/3/20 delay fix
+    String url = "apirosreestr.ru";
+    boolean reachable = RestService.isReachable(url, 443, 2000);
+    log.info(url + " isReacheble - " + reachable);
+    delay = delay(url);
+    log.info("Delay cron "+ url,delay);
+    Status.updateStatusServer(delay, reachable, false);
+  }
+
+  private long delay(String url){
+    // in case of Linux change the 'n'(Mac) to 'c'(Linux)
+    Process p1 = null;
+    try {
+      p1 = Runtime.getRuntime().exec("ping -c 1 " + url);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    BufferedReader inputStream = new BufferedReader(new InputStreamReader(p1.getInputStream()));
+    String s = "";
+    List<String> list = new ArrayList<>();
+    // reading output stream of the command
+    while (true) {
+      try {
+        if ((s = inputStream.readLine()) == null) break;
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      list.add(s);
+    }
+    System.out.println(list);
+    System.out.println(list.get(1));
+    try{
+      String split = list.get(1).split("time=")[1];
+      System.out.println(split);
+      return (long) Double.parseDouble(split.substring(0, split.length() - 3));
+    } catch (ArrayIndexOutOfBoundsException e) {
+      return 3000;
+    }
+
+
   }
 }
